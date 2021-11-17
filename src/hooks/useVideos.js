519474afbf79f8e27ddc2e7ec68a65acfa5b390c
RJ060501 <rjrussell605@gmail.com>
2021-11-17 12:37:42 -0700

    ready for deploy

---
+++ src/hooks/useVideos.js
@@ -0,0 +1,24 @@
+import { useState, useEffect } from "react";
+import youtube from "../apis/youtube";
+
+const useVideos = (defaultSearchTerm) => {
+  const [videos, setVideos] = useState([]);
+  useEffect(() => {
+    search(defaultSearchTerm);
+//doing what react asks from console.
+  }, [defaultSearchTerm]);
+
+  const search = async (term) => {
+    const response = await youtube.get("/search", {
+      params: {
+        q: term,
+      },
+    });
+
+    setVideos(response.data.items);
+  };
+
+  return [videos, search];
+};
+
+export default useVideos;
